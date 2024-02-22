4a3e6bc807b3c887efe951c0bf115451ed68b9e8
Wilfried Fosse <wilfriedfosse2@gmail.com>
2024-02-22 14:08:06 +0100

    donnne

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+10.times do
+  Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: Faker::Internet.url,
+    rating: rand(1..10)
+  )
+end
+puts "Created #{Movie.count} movies."
+# Créer des listes
+10.times do
+  List.create!(
+    name: Faker::Book.title
+  )
+end
+puts "Created #{List.count} lists."
+if Movie.any? && List.any?
+  10.times do
+    Bookmark.create!(
+      comment: Faker::Lorem.sentence(word_count: 5),
+      movie: Movie.all.sample,
+      list: List.all.sample
+    )
+  end
+  puts "Created #{Bookmark.count} bookmarks."
+else
+  puts 'No movies or lists found. Bookmarks were not created.'
+end
