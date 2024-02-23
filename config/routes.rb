fc41be62cff457612ced6d74f5c1d7a85681b1cb
Wilfried Fosse <wilfriedfosse2@gmail.com>
2024-02-23 10:57:29 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
-  root 'lists#index'
-  resources :lists do
-    resources :bookmarks, only: [:index, :new, :create]
+  # root 'lists#index'
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: :destroy
 end
